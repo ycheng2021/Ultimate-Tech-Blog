c8466b43516ba3657662849cb4fb9f468f11e82d
Yang Cheng <annacheng09@hotmail.com>
2022-04-07 21:26:59 -0700

    added post and user models

---
+++ models/post.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        contents: {
+            type: DataTypes.STRING,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        }
+    }
+)
