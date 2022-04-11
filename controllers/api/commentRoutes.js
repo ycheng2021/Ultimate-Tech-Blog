d95e5e1a3b7cad2d5a0892c3f95b9d193ffa63bc
Yang Cheng <annacheng09@hotmail.com>
2022-04-11 15:39:16 -0700

    added develop branch

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,1 +1,19 @@
-// post route for comment
+const router = require("express").Router();
+const { Post } = require("../models");
+
+// post a comment
+
+router.post("/", async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+    //   post_id: req.session.post_id,
+    });
+
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
