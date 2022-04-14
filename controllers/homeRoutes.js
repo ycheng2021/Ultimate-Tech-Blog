dc4c00ae54e40977395ab973de652439d7c5e21e
Yang Cheng <annacheng09@hotmail.com>
2022-04-14 15:14:10 -0700

    split login and signup

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -16,22 +16,33 @@ router.get('/', async (req, res) => {
             model: Comment,
             attributes: ['contents']
           }
-        ],
+        ]
       });
   
       // Serialize data so the template can read it
       const posts = postData.map((post) => post.get({ plain: true }));
-  
+
       // Pass serialized data and session flag into template
       res.render('homepage', { 
         posts, 
         logged_in: req.session.logged_in 
       });
-      res.json(posts)
+      // res.json(posts)
     } catch (err) {
       res.status(500).json(err);
     }
   });
+
+// get the page to sign up
+router.get('/signup', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
 
 // get post by id 
 
--- public/js/login.js
+++ public/js/login.js
@@ -24,35 +24,9 @@ const loginFormHandler = async (event) => {
       }
     }
   };
-  // signup function
-  const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (name && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ name, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
   
   // addEventListeners for sumbit buttons
   document
     .querySelector('.login-form')
     .addEventListener('submit', loginFormHandler);
-  
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
   
