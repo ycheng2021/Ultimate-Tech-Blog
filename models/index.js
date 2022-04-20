77ec3f57aa07e8aa399f024d2f839703a10af9da
Yang Cheng <annacheng09@hotmail.com>
2022-04-19 17:50:34 -0700

    changed the model, trying to deploy

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
-const Post = require('./post');
 const User = require('./user');
+const Post = require('./post');
 const Comment = require('./comment')
 
 User.hasMany(Post, {
@@ -7,22 +7,21 @@ User.hasMany(Post, {
     onDelete: 'CASCADE'
 });
 
-Post.hasMany(Comment, {
-    foreignKey: 'post_id',
-    onDelete: 'CASCADE'
-})
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
 User.hasMany(Comment, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 })
 
-Post.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-});
-
 Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+})
+
+Post.hasMany(Comment, {
     foreignKey: 'post_id',
     onDelete: 'CASCADE'
 })
